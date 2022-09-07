13d667e3ed936cd10bcb00ddb149c03c78dd75fc
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-09-07 19:49:13 +0200

    Shitty crap!

---
+++ C++ Class/Main.cpp
@@ -0,0 +1,14 @@
+/*
+
+	This is only a kind of template, to make sure stuff compiles.
+	However if you start a TeddyBear based project yourself, I guess this can come in handy (somehow)
+
+*/
+
+
+#include "headers/SuperTed_Core.hpp"
+
+
+int main(int cnt, char* args) {
+	return 0;
+}
