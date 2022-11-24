000a090ba53a8c086163cf34eded741f82665fd8
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-11-24 15:51:35 +0100

    Update SupJCR.cpp

--- SupJCR/SupJCR.cpp
+++ SupJCR/SupJCR.cpp
@@ -46,9 +46,10 @@ namespace SuperTed {
 				QCol->Doing("Initializing", "JCR6 zlib driver"); init_zlib();
 				QCol->Doing("Analyzing", "SuperTed.JCR");
 				STEDA = Dir(d + "/SuperTed.JCR");
-				STEDA_ID.Parse(STEDA.String("ID/ID.inig"));
-				QCol->Doing("JCR file build", STEDA_ID.Value("Build", "Date"));
+				STEDA_ID.Parse(STEDA.String("ID/ID.ini"));
+				QCol->Doing("Checking", "SuperTed.JCR");
 				if (Lower(STEDA_ID.Value("ID", "Sig")) != "fbeb79a1") { QCol->Error("SuperTed.JCR signature incorrect!"); exit(255); }
+				QCol->Doing("JCR file build", STEDA_ID.Value("Build", "Date"));
 			}
 			return &STEDA;
 		}
