fe7af0c4d2de845260b152f2ed5ef20b80219436
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-11-24 19:16:39 +0100

    UI Creation (unfinished)

--- Launcher/Run.cpp
+++ Launcher/Run.cpp
@@ -29,13 +29,18 @@
 #include <TQSE.hpp>
 #include <TQSG.hpp>
 
+#include <june19.hpp>
+
 #include <QCol.hpp>
 #include <TrickyMath.hpp>
 #include <QuickString.hpp>
+#include <QuickStream.hpp>
+#include <FileList.hpp>
 
 #include "Glob.hpp"
 #include "Run.hpp"
 
+using namespace june19;
 using namespace TrickyUnits;
 
 namespace SuperTed {
@@ -46,6 +51,114 @@ namespace SuperTed {
 		static void Err(std::string e) {
 			ErrorThrown = true;
 			QCol->Error(e);
+		}
+
+		static j19gadget* UI{ nullptr };
+		static j19gadget* selPrj{ nullptr };
+		static j19gadget* haveprj{ nullptr };
+		static j19gadget* selMap{ nullptr };
+		static j19gadget* newMap{ nullptr };
+		static j19gadget* newMapAct{ nullptr };
+
+		static int Exe(std::string cmd) {
+			QCol->Doing("Executing", cmd);
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+			cmd = TReplace(cmd, '/', '\\'); // Guarantee cmd support in Windows!
+#endif
+			TQSG_Cls();
+			TQSG_Color(255, 0, 0);
+			j19gadget::GetDefaultFont()->Draw("External call in session...", floor(TQSG_ScreenWidth() / 2), floor(TQSG_ScreenHeight() / 2), 2, 2);
+			TQSG_Flip();
+			auto ret = system(cmd.c_str());
+			return ret;
+
+		}
+
+		static void ActNewProject(j19gadget*j,j19action a){ 
+			std::string cmd{ MyDir + "/NewProjectWizard.exe" };
+			if (!FileExists(cmd)) {
+				TQSE_Notify("The wizard (" + cmd + ") could not be found");
+				return;
+			}
+			Exe(cmd);
+		}
+
+		static void PCheck(j19gadget* j, j19action a){
+			if (!haveprj) return;
+			haveprj->Visible = selPrj->ItemText()!="";
+		}
+
+		static void ActRenewProjectList(j19gadget* j, j19action a) {
+			auto List = FileList(ProjectsDir(), Directories);
+			selPrj->ClearItems();
+			for (auto F : List) selPrj->AddItem(F);
+			PCheck(j, a);
+		}
+
+		static void ActNew(j19gadget*, j19action a) {
+			QCol->Error("Action for new map not yet implemented");
+		}
+
+		static void TypeNew(j19gadget* j, j19action a) {
+			switch (a) {
+			case j19action::Activate:
+				break;
+			case j19action::Enter:
+				ActNew(j, a);
+				break;
+			case j19action::Type:
+			case j19action::BackSpace:
+				newMapAct->Enabled = newMap->Text.size();
+				break;
+			default:
+				QCol->Error("Unknown action on NewMapName Gadget (" + std::to_string((int)a)+")");
+				QCol->Pink("Please notify Jeroen P. Broks about this, as you likely encountered a bug!");
+				break;
+			}
+		}
+
+		void CreateUI() {
+			QCol->Doing("Configuring", "Font");
+			j19gadget::SetDefaultFont(JAS, "Fonts/DOS.jfbf");
+			QCol->Doing("Creating", "User Interface");
+			UI = WorkScreen();
+			auto h = UI->H(), dh = (int)ceil(h * 0.25);
+			auto w = UI->W(), dw = w - 250;
+			auto labPrj{ CreateLabel("Projects",0,0,40,20,UI) };
+			labPrj->FR = 255; labPrj->FG = 180; labPrj->FB = 0; labPrj->FA = 255;
+			selPrj= CreateListBox(5,20,dw - 10,dh - 20,UI) ;
+			selPrj->FR = 255; selPrj->FG = 180; selPrj->FB = 0; selPrj->FA = 255;
+			selPrj->BR = 25; selPrj->BG = 18; selPrj->BB = 0; selPrj->BA = 255;
+			selPrj->CBAction = PCheck;
+			auto newPrj = CreateButton("New Project", dw, 5, UI);
+			newPrj->CBAction = ActNewProject;
+			newPrj->FR = 255; newPrj->FG = 180; newPrj->FB = 0; newPrj->FA = 255;
+			newPrj->BR = 25; newPrj->BG = 18; newPrj->BB = 0; newPrj->BA = 255;
+			auto renPrj = CreateButton("Renew Project List", dw, 30, UI);
+			renPrj->FR = 255; renPrj->FG = 180; renPrj->FB = 0; renPrj->FA = 255;
+			renPrj->BR = 25; renPrj->BG = 18; renPrj->BB = 0; renPrj->BA = 255;
+
+			renPrj->CBAction = ActRenewProjectList;
+			ActRenewProjectList(nullptr, j19action::Unknown);
+
+			haveprj = CreateGroup(0, dh, dw, UI->H() - dh, UI);
+			haveprj->Visible = false;
+			auto labMap{ CreateLabel("Maps",0,0,40,20,haveprj,0) };
+			labMap->FR =0; labMap->FG = 180; labMap->FB = 0; labMap->FA = 255;
+			selMap = CreateListBox(5, 20, dw, haveprj->H() - 60, haveprj);
+			selMap->FR = 0; selMap->FG = 180; selMap->FB = 255; selMap->FA = 255;
+			selMap->BR = 0; selMap->BG = 18; selMap->BB = 25; selMap->BA = 255;
+
+			auto newy{ haveprj->H() - 30 };
+			newMap = CreateTextfield(5, newy, dw-10, haveprj);
+			newMap->FB = 0; newMap->FR = 180; newMap->FG = 255; newMap->FA = 255;
+			newMap->BB = 0; newMap->BR = 18; newMap->BG = 25; newMap->BA = 255;
+			newMap->CBAction = TypeNew;
+			newMapAct = CreateButton("Create new map", dw, newy, haveprj);
+			newMapAct->FB = 0; newMapAct->FR = 180; newMapAct->FG = 255; newMapAct->FA = 255;
+			newMapAct->BB = 0; newMapAct->BR = 18; newMapAct->BG = 25; newMapAct->BA = 255;
+			newMapAct->Enabled = false;
+
 		}
 
 		void LoadBackGround() {
@@ -64,14 +177,16 @@ namespace SuperTed {
 			static int r{ 0 };
 			TQSE_Poll();
 			TQSG_Cls();
+			TQSG_Color(255, 255, 255);
 			Background->Tile(0, 0, TQSG_ScreenWidth(), TQSG_ScreenHeight(), floor((TQSG_ScreenWidth() / 2) + (DegSin(r) * 250)), floor((TQSG_ScreenHeight() / 2) + (DegCos(r) * 250)));
 			r = (r + 1) % 360;
-
+			UI->Draw();
 			KeepGoing = KeepGoing && !TQSE_Quit();
 #ifdef ESCAPE_IS_QUIT
 			KeepGoing = KeepGoing && !TQSE_KeyHit(SDLK_ESCAPE);
 #endif
 			TQSG_Flip(30);
+			if (!KeepGoing) FreeGadget(UI);
 			return KeepGoing;
 		}
 	}
