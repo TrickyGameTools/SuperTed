ddf4cba5dfb3238a54bb393a5a4029d4b0794851
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-11-24 14:15:12 +0100

    Build JCR

---
+++ BJCR.ps1
@@ -0,0 +1,24 @@
+# Build JCR
+
+function private:builddate{                           
+        $date = Get-Date                              
+        $ginie = '# Identification data      
+                                                      
+[Build]                                  
+Date=' + $date + '        
+		
+[ID]
+For=SuperTed
+Sig=fbeb79a1
+
+'                                             
+                                                      
+        echo $ginie > ID/ID.ini
+}                                                     
+
+pushd
+cd JCR
+builddate
+del ../Release/SuperTed.JCR
+NJCR add ../Release/SuperTed.JCR  -fc zlib -cm zlib
+popd
