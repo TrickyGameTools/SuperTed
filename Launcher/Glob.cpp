54a78d4f29355e97763124d4b954f4699801812f
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-11-25 18:06:58 +0100

    vUpdate

--- Launcher/Glob.cpp
+++ Launcher/Glob.cpp
@@ -21,7 +21,7 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 22.11.24
+// Version: 22.11.25
 // EndLic
 
 #include <string>
