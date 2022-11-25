96454bc21b395d0776b8f2260f8ebe967df450a7
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-11-25 13:42:18 +0100

    Import Lua
    (And I made all drives to be replaced by $drive. If Windows spooks up my drive letters again (which it recently did) I only have to change 1 single line to get things working again).

--- impdll.ps1
+++ impdll.ps1
@@ -18,22 +18,26 @@ function private:imp($want) {
         cp "$want" Release
 }
 
-echo "Pre-build script - (c) Jeroen P. Broks 2022"
+echo "DLL Import Script - (c) Jeroen P. Broks 2022"
 
 #builddate
 #../../BuildJCR/bin/Release/net6.0/BuildJCR.exe
 
-imp "E:\projects\applications\visualstudio\vc\zlib-msvc14-x64.1.2.11.7795\build\native\bin_release\zlib.dll"
-imp "E:\projects\applications\visualstudio\vc\3rdparty\SDL2-2.0.12\lib\x64\SDL2.dll"
-imp "E:\projects\applications\visualstudio\vc\3rdparty\SDL2_image-2.0.5\lib\x64\SDL2_image.dll"
-#imp "E:\projects\applications\visualstudio\vc\3rdparty\SDL2_image-2.0.5\lib\x64\libpng16-16.dll"
-imp "E:\projects\applications\visualstudio\vc\3rdparty\SDL2_image-2.0.5\lib\x64\lib*.dll"
-imp "E:\projects\applications\visualstudio\vc\3rdparty\SDL2_image-2.0.5\lib\x64\LIC*.txt"
-imp "E:\projects\applications\visualstudio\vc\3rdparty\SDL2_image-2.0.5\lib\x64\zlib1.dll"
-imp "E:\projects\applications\visualstudio\vc\3rdparty\SDL2_mixer-2.0.4\lib\x64\SDL2_mixer.dll"
-imp "E:\projects\applications\visualstudio\vc\3rdparty\SDL2_mixer-2.0.4\lib\x64\libogg-0.dll"
-imp "E:\projects\applications\visualstudio\vc\3rdparty\SDL2_mixer-2.0.4\lib\x64\libvorbis-0.dll"
-imp "E:\projects\applications\visualstudio\vc\3rdparty\SDL2_mixer-2.0.4\lib\x64\libvorbisfile-3.dll"
+$drive="E:"
+
+imp "$drive/projects/applications/visualstudio/vc/zlib-msvc14-x64.1.2.11.7795/build/native/bin_release/zlib.dll"
+imp "$drive/projects/applications/visualstudio/vc/3rdparty/SDL2-2.0.12/lib/x64/SDL2.dll"
+imp "$drive/projects/applications/visualstudio/vc/3rdparty/SDL2_image-2.0.5/lib/x64/SDL2_image.dll"
+#imp "$drive/projects/applications/visualstudio/vc/3rdparty/SDL2_image-2.0.5/lib/x64/libpng16-16.dll"
+imp "$drive/projects/applications/visualstudio/vc/3rdparty/SDL2_image-2.0.5/lib/x64/lib*.dll"
+imp "$drive/projects/applications/visualstudio/vc/3rdparty/SDL2_image-2.0.5/lib/x64/LIC*.txt"
+imp "$drive/projects/applications/visualstudio/vc/3rdparty/SDL2_image-2.0.5/lib/x64/zlib1.dll"
+imp "$drive/projects/applications/visualstudio/vc/3rdparty/SDL2_mixer-2.0.4/lib/x64/SDL2_mixer.dll"
+imp "$drive/projects/applications/visualstudio/vc/3rdparty/SDL2_mixer-2.0.4/lib/x64/libogg-0.dll"
+imp "$drive/projects/applications/visualstudio/vc/3rdparty/SDL2_mixer-2.0.4/lib/x64/libvorbis-0.dll"
+imp "$drive/projects/applications/visualstudio/vc/3rdparty/SDL2_mixer-2.0.4/lib/x64/libvorbisfile-3.dll"
+
+imp "$drive/Projects/Applications/VisualStudio/VC/3rdparty/Lua/lua-5.4.0/Lua Library/x64/Release/Lua.dll"
 
 echo "
 Done
