b6c39d49bc9bb91644ff041187ff7563da50b23c
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-05-12 12:11:45 +0200

    vUpdate

--- Editor/MapEdit.cpp
+++ Editor/MapEdit.cpp
@@ -21,7 +21,7 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 23.05.11
+// Version: 23.05.12
 // EndLic
 #include <SlyvQCol.hpp>
 #include <SlyvSTOI.hpp>
