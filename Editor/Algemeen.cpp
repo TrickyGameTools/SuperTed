f9c56dcf62bea6edb8a0471644d4456729db1dd7
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-05-12 11:39:33 +0200

    Several compatibility things now taken care of

--- Editor/Algemeen.cpp
+++ Editor/Algemeen.cpp
@@ -21,7 +21,7 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 23.05.11
+// Version: 23.05.12
 // EndLic
 
 #include <SlyvQCol.hpp>
@@ -38,6 +38,7 @@ using namespace Slyvina;
 using namespace June19;
 using namespace Units;
 using namespace TQSG;
+using namespace TQSE;
 
 namespace Slyvina {
 	namespace SuperTed {
@@ -63,7 +64,7 @@ namespace Slyvina {
 				if (Stage.count(st)) Crash("Dupe stage: " + st);
 				Stage[st] = std::make_shared<_UI>();
 				Stage[st]->_Name = st;
-				Stage[st]->MainGadget = CreateGroup(0, 0, TQSG_ScreenWidth(), TQSG_ScreenHeight() - 36, WorkScreen());
+				Stage[st]->MainGadget = CreateGroup(0, 0, ScreenWidth(), ScreenHeight() - 36, WorkScreen());
 			}
 
 			UI _UI::GetStage(std::string st) {
@@ -96,7 +97,7 @@ namespace Slyvina {
 				// JCR6
 				//auto J{ JAS };
 
-				jcr6::JCRPanic = JPanic;
+				JCR6::JCR6PANIC = JPanic;
 
 
 				// Interface load
@@ -104,10 +105,10 @@ namespace Slyvina {
 				QCol->Doing("Staring", "User Interface");
 
 				// Mouse
-				TQSE_Init();
+				//TQSE_Init();
 				HideMouse();
-				jcr6::JT_Dir* JD{ JAS };
-				Mouse = TQSG_LoadAutoImage(JD, std::string("Img/Mouse.png"));
+				JCR6::JT_Dir JD{ JAS };
+				Mouse = LoadImage(JD, std::string("Img/Mouse.png"));
 
 				// SuperTed
 				// TODO: Init SuperTed and the TQSG driver
@@ -164,18 +165,18 @@ namespace Slyvina {
 				auto CS{ CurrentStage() };
 				//auto go_on{ true };
 				go_on = true;
-				TQSG_Cls();
+				Cls();
 				if (!CS) { Throw("No stage"); return false; }
 				if (CS->PreJune) CS->PreJune();
 				//TQSE_Poll();
 				Screen()->Draw();
 				if (CS->PostJune) CS->PostJune();
-				if (TQSE_Quit()) go_on = false;
+				if (TQSE::AppTerminate()) go_on = false;
 #ifdef QUICK_QUIT
 				if (TQSE_KeyHit(SDLK_ESCAPE)) go_on = false;
 #endif
 				SetColor(255, 255, 255, 255);
-				Mouse->Draw(TQSE_MouseX(), TQSE_MouseY());
+				Mouse->Draw(MouseX(), MouseY());
 				Flip(20);
 				return go_on;
 			}
